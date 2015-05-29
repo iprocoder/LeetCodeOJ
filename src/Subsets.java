dd35e54a04f3fd773f911affdd0ec9b24079079d
iprocoder <iprocoder@gmail.com>
2015-05-29 13:01:26 +0800

    Subsets

--- src/Subsets.java
+++ src/Subsets.java
@@ -25,6 +25,6 @@ public class Subsets {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Subsets().subsets(new int[]{1, 5, 3}));
+        System.out.println(new Subsets().subsets(new int[]{1, 2, 2}));
     }
 }
